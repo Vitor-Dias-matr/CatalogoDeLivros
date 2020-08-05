d895f419f69b41f5a10477f72a06e4e571f4fb3c
Vitor Dias Martins <vitor17martins@hotmail.com>
2020-08-05 18:01:06 -0400

    subindo projeto

--- ApiCaalogoLivros/Repository/LivroRepository.cs
+++ ApiCaalogoLivros/Repository/LivroRepository.cs
@@ -13,11 +13,6 @@ namespace ApiCaalogoLivros.Repository
         public List<Livros> Query()
         {
             var json = File.ReadAllText(@"../ApiCaalogoLivros/Livros.json", Encoding.GetEncoding("iso-8859-1"));
-
-            // var json = new WebClient().DownloadString("https://raw.githubusercontent.com/Vitor-Dias-matr/BackendTest/master/books.json");
-
-            //var cliente = new WebClient();
-            //var json = cliente.DownloadString("https://raw.githubusercontent.com/Vitor-Dias-matr/BackendTest/master/books.json");
                             
             var livros = JsonConvert.DeserializeObject<List<Livros>>(json);
                                        
